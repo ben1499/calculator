10696b1f8ffcf4235d93f3fb7bd2cfedc9ccd7ce
Reuben <reubenthomas99@gmail.com>
2023-04-25 12:15:43 +0530

    Create individual operator functions and the operate function

---
+++ main.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+': return add(a, b);;
+        case '-': return subtract(a, b);;
+        case '*': return multiply(a, b);;
+        case '/': return divide(a, b);
+    }
+}
+
