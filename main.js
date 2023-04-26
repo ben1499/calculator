da5b31667c280ad89aab4f0fca87ebab2497f009
Reuben <reubenthomas99@gmail.com>
2023-04-26 18:50:58 +0530

    Refactor populate display function

--- main.js
+++ main.js
@@ -27,3 +27,23 @@ function operate(operator, a, b) {
     }
 }
 
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+function populateDisplay(content) {
+    display.textContent = content;
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (this.classList.contains('number')) {
+            //let output = this.value;
+            display.textContent += this.value;
+        } else if (this.classList.contains('operator')) {
+            let firstNum = +(display.textContent);
+            console.log(firstNum);  
+        } else if (this.classList.contains('equals')) {
+    
+        }
+    });
+});
